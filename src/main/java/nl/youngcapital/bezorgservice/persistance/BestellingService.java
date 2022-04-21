5c1a27a5cad7c45255a9e4797bf5cf4464f09d3a
yifongau <yifongau@gmail.com>
2022-04-21 13:35:22 +0200

    bestellingservice afgerond

--- src/main/java/nl/youngcapital/bezorgservice/persistance/BestellingService.java
+++ src/main/java/nl/youngcapital/bezorgservice/persistance/BestellingService.java
@@ -4,10 +4,30 @@
  */
 package nl.youngcapital.bezorgservice.persistance;
 
+import nl.youngcapital.bezorgservice.domein.Bestelling;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author yifongau
  */
+
+@Service
 public class BestellingService {
     
+    @Autowired
+    BestellingRepository bestellingRepository;
+    
+    public void opslaanBestelling(Bestelling b) {
+        bestellingRepository.save(b);
+    }
+    
+    public Iterable<Bestelling> geefBestellingen(){
+        return bestellingRepository.findAll();
+}
+    public Bestelling vindBestellingById(long bid){
+        Bestelling gevonden = bestellingRepository.findById(bid).get();
+        return gevonden;
+    }
 }
