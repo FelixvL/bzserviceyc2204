656665aa9257eed16e89053659b1cb28e1ccd6a1
yifongau <yifongau@gmail.com>
2022-04-21 12:54:01 +0200

    refactor persistance + bestelling klasse

---
+++ src/main/java/nl/youngcapital/bezorgservice/domein/Bestelling.java
@@ -0,0 +1,14 @@
+package nl.youngcapital.bezorgservice.domein;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author yifongau
+ */
+public class Bestelling {
+    
+}
