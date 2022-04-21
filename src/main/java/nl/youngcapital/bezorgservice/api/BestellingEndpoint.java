04edee4550e2cfeefa4496fadc4072d87a6da18c
yifongau <yifongau@gmail.com>
2022-04-21 13:47:14 +0200

    bestelling endpoints toegevoegd.

--- src/main/java/nl/youngcapital/bezorgservice/api/BestellingEndpoint.java
+++ src/main/java/nl/youngcapital/bezorgservice/api/BestellingEndpoint.java
@@ -4,15 +4,37 @@
  */
 package nl.youngcapital.bezorgservice.api;
 
+import nl.youngcapital.bezorgservice.domein.Bestelling;
+import nl.youngcapital.bezorgservice.persistance.BestellingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author yifongau
  */
+
+@RestController
 public class BestellingEndpoint {
     
     @Autowired
-    BestellingService bestelllingservice
+    BestellingService bestellingService;
             
-    @GetMapping("geefallebestellingen")
-            public Iterable Geef 
+    @PostMapping("/nieuwebestelling")
+        public void voegbestellingtoe(@RequestBody Bestelling b){
+            bestellingService.opslaanBestelling(b);
+        }
+    @GetMapping("/geefallebestellingen")
+            public Iterable GeefAlleBestellingen(){
+                return bestellingService.geefBestellingen();
+            }
+            
+    @GetMapping("/geefbestelling/{bid}")
+    public Bestelling bestellingbyid(@PathVariable("bid") int bezorgerid) {
+        return bestellingService.vindBestellingById(bezorgerid);
+    }
 }
