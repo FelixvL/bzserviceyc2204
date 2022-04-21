1798994e3a3dd62e106368c55a306b07dcd73e65
Lunziq <kuiperswessel95@gmail.com>
2022-04-21 11:32:19 +0200

    Eerste backend commit

---
+++ src/main/java/nl/youngcapital/bezorgservice/api/BezorgerEndpoint.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nl.youngcapital.bezorgservice.api;
+
+/**
+ *
+ * @author kuipe
+ */
+public class BezorgerEndpoint {
+    
+}
