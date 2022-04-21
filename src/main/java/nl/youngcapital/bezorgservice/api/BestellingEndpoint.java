f040b34b4488b66a9c48104edd8125f959da3734
yifongau <yifongau@gmail.com>
2022-04-21 13:19:37 +0200

    blanco bestellingketen

---
+++ src/main/java/nl/youngcapital/bezorgservice/api/BestellingEndpoint.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nl.youngcapital.bezorgservice.api;
+
+/**
+ *
+ * @author yifongau
+ */
+public class BestellingEndpoint {
+    
+    @Autowired
+    BestellingService bestelllingservice
+            
+    @GetMapping("geefallebestellingen")
+            public Iterable Geef 
+}
