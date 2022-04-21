5f02c9f5f1b3af3a966e003f6c70fb9bbc60ad0b
FelixvL <digitaleservice@gmail.com>
2022-04-21 10:35:09 +0200

    eerste opzet

---
+++ src/main/java/nl/youngcapital/bezorgservice/BezorgserviceApplication.java
@@ -0,0 +1,13 @@
+package nl.youngcapital.bezorgservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezorgserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezorgserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/youngcapital/bezorgservice/BezorgserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.youngcapital.bezorgservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezorgserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
