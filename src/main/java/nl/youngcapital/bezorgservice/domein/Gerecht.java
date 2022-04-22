7848d380227f39c5bd611c1da21e7ee9553d2a52
attenicas <thomas.welten@gmx.net>
2022-04-22 15:08:32 +0200

    toevoegen Gerecht aan menu

---
+++ src/main/java/nl/youngcapital/bezorgservice/domein/Gerecht.java
@@ -0,0 +1,42 @@
+package nl.youngcapital.bezorgservice.domein;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Gerecht {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+	String naam;
+	int prijs;
+	String afbeelding;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNaam() {
+		return naam;
+	}
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+	public int getPrijs() {
+		return prijs;
+	}
+	public void setPrijs(int prijs) {
+		this.prijs = prijs;
+	}
+	public String getAfbeelding() {
+		return afbeelding;
+	}
+	public void setAfbeelding(String afbeelding) {
+		this.afbeelding = afbeelding;
+	}
+
+}
