12ac273b95501fec9fc51e49b086c89a336595d9
Raymond Loman <raymond@carpago.nl>
2022-05-02 15:25:20 +0200

    More refactoring of the other endpoints

--- src/main/java/nl/youngcapital/bezorgservice/api/BestellingEndpoint.java
+++ src/main/java/nl/youngcapital/bezorgservice/api/BestellingEndpoint.java
@@ -4,15 +4,17 @@
  */
 package nl.youngcapital.bezorgservice.api;
 
-import nl.youngcapital.bezorgservice.domein.Bestelling;
-import nl.youngcapital.bezorgservice.persistance.BestellingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import nl.youngcapital.bezorgservice.domein.Bestelling;
+import nl.youngcapital.bezorgservice.persistance.BestellingService;
 
 /**
  *
@@ -20,44 +22,40 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("api/bestellingen")
 public class BestellingEndpoint {
 
 	@Autowired
-	BestellingService bestellingService;
+	private BestellingService bestellingService;
 
-	@PostMapping("/nieuwebestelling/{klantid}/{restaurantid}")
-	public void voegbestellingtoe(@RequestBody Bestelling b, @PathVariable("klantid")long id, @PathVariable("restaurantid")long rid) {
+	@PostMapping("klant/{klantid}/restaurant/{restaurantid}")
+	public void voegbestellingtoe(@RequestBody Bestelling b, @PathVariable("klantid") long id,
+			@PathVariable("restaurantid") long rid) {
 		bestellingService.opslaanBestelling(b, id, rid);
 	}
-        
-        @PutMapping("voeggerechttoe/{bestellingid}/{gerechtid}")
-        public void voegGerechtToe(@PathVariable("bestellingid") long bid, @PathVariable("gerechtid")long id)
-        {
-            bestellingService.voegGerechtToe(bid, id);
-        }
-        
-                
-	@GetMapping("/geefallebestellingen")
-	public Iterable<Bestelling> geefAlleBestellingen(){
+
+	@PutMapping("voeggerechttoe/{bestellingid}/{gerechtid}")
+	public void voegGerechtToe(@PathVariable("bestellingid") long bid, @PathVariable("gerechtid") long id) {
+		bestellingService.voegGerechtToe(bid, id);
+	}
+
+	@GetMapping
+	public Iterable<Bestelling> geefAlleBestellingen() {
 		return bestellingService.geefBestellingen();
 	}
-        
-        @GetMapping("geefbestellingenvanklant/{kid}")
-        public Iterable<Bestelling> geefBestellingenVanKlant(@PathVariable("kid") long klantid)
-         {
-             return bestellingService.geefBestellingVanKlant(klantid);
-         }
 
-	@GetMapping("/geefbestelling/{bid}")
-	public Bestelling bestellingbyid(@PathVariable("bid") int bezorgerid) {
-		return bestellingService.vindBestellingById(bezorgerid);
+	@GetMapping("geefbestellingenvanklant/{klantId}") // als de naam van de PathVariable (hier klantId) gelijk is aan de parameter hieronder (in de signature van de method, dan mag je de value onder @PathVariable leeg laten.
+	public Iterable<Bestelling> geefBestellingenVanKlant(@PathVariable long klantId) { // kijk, hier is klantId boven de method in de annotatie en als parameter dus hetzelfde.
+		return bestellingService.geefBestellingVanKlant(klantId);
+	}
+
+	@GetMapping("/geefbestelling/{bezorgerId}")
+	public Bestelling findBestellingByBezorgerId(@PathVariable int bezorgerId) {
+		return bestellingService.vindBestellingById(bezorgerId);
 	}
 
 	@PostMapping("/addbestelling/{klantid}")
-	public void addBestelling(@RequestBody Bestelling b)
-	{
-           
-
+	public void addBestelling(@RequestBody Bestelling b) {
+		//TODO
 	}
-
 }
--- src/main/java/nl/youngcapital/bezorgservice/api/DocentEndpoint.java
+++ src/main/java/nl/youngcapital/bezorgservice/api/DocentEndpoint.java
@@ -5,15 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.youngcapital.bezorgservice.domein.Docent;
 import nl.youngcapital.bezorgservice.persistance.DocentService;
 
 @RestController
+@RequestMapping("api/docenten")
 public class DocentEndpoint {
 	@Autowired
-	DocentService ds;
+	private DocentService ds;
 	
 	
 	@GetMapping("/docent")
@@ -22,17 +24,17 @@ public class DocentEndpoint {
 		return new Docent();
 	}
 	
-	@PostMapping("nieuweDocent")
+	@PostMapping
 	public void nieuweDocent(@RequestBody Docent docent) {
 		ds.saveDocent(docent);
 	}
 	
-	@GetMapping("geefDocent/{id}")
+	@GetMapping("{id}")
 	public Docent geefDocent(@PathVariable("id") int id ) {
 		return ds.geefDocent(id);
 	} 
 
-	@GetMapping("geefAlleDocenten")
+	@GetMapping
 	public Iterable<Docent> geefAlleDocenten(){
 		return ds.geefAlleDocenten();
 	}
