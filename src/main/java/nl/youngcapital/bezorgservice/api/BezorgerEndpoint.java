cbc8262e68519a19154c75157ef06bd23eb4afc0
Raymond Loman <raymond@carpago.nl>
2022-05-02 10:39:23 +0200

    Refactor BezorgerEndpoint

--- src/main/java/nl/youngcapital/bezorgservice/api/BezorgerEndpoint.java
+++ src/main/java/nl/youngcapital/bezorgservice/api/BezorgerEndpoint.java
@@ -1,31 +1,29 @@
 package nl.youngcapital.bezorgservice.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import nl.youngcapital.bezorgservice.domein.Bezorger;
 import nl.youngcapital.bezorgservice.persistance.BezorgerService;
 
 @RestController
+@RequestMapping("api/bezorgers") // default api/bezorgers
 public class BezorgerEndpoint {
+
 	@Autowired
 	BezorgerService bs;
-	
-	@GetMapping("/bezorgerbyid/{id}")
+
+	@GetMapping("{id}")  // voegt {id} toe aan api/bezorgers. bv. http://localhost:8080/api/bezorgers/3
 	public Bezorger bezorgerbyid(@PathVariable("id") int bezorgerid) {
 		return bs.vindBezorgerById(bezorgerid);
 	}
-	
-	@GetMapping("/toonbezorgers") 
-	public Iterable<Bezorger> toonbezorgers(){
+
+	@GetMapping // Getmapping dus toont alle bezorgers (default mapping: api/bezorgers)
+	public Iterable<Bezorger> toonbezorgers() {
 		return bs.geefBezorgers();
 	}
-	
-	@PostMapping("/bezorgertoevoegen")
+
+	@PostMapping // Post is standaard voor toevoegen dus POST naar api/bezorgers is impliciet een niewe maken.
 	public void voegbezorgertoe(@RequestBody Bezorger b) {
 		bs.opslaan(b);
 	}
